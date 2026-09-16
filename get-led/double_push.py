675a3786bb7f7a73bad48ec53e7940e944ea080d
kirilivasml-glitch <kiril.iva.sml@gmail.com>
2026-09-16 09:09:07 +0300

    New tasks

---
+++ get-led/double_push.py
@@ -0,0 +1,61 @@
+import time
+import RPi.GPIO as GPIO
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+def turn_the_on(leds, num_2):
+    for value in str(num_2):
+        i = 0
+        GPIO.output(leds[i] ,int(value))
+        i+=1
+
+
+
+
+GPIO.setmode(GPIO.BCM)
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+up = 10
+down = 9
+
+GPIO.setup(leds, GPIO.OUT)
+
+GPIO.output(leds, 0)
+
+GPIO.setup(up,down, GPIO.IN)
+
+num = 0
+
+sleep_time = 0.2
+
+if GPIO.input(up):
+   if num<256:
+    num+=1
+    print(num, dec2bin(num))
+
+    turn_the_on(leds, dec2bin(num) )
+    time.sleep(sleep_time)
+   else:
+       num = 0
+       print(num, dec2bin(num))
+       turn_the_on(leds, dec2bin(num))
+       time.sleep(sleep_time)
+
+if GPIO.input(down):
+      if num>0:
+          num-=1
+          print(num, dec2bin(num))
+          turn_the_on(leds, dec2bin(num))
+          time.sleep(sleep_time)
+      else:
+          n = 0
+          print(num, dec2bin(num))
+          turn_the_on(leds, dec2bin(num))
+          time.sleep(sleep_time)
+
+if GPIO.input(down) and GPIO.input(up):
+    num = 255
+    print(num, dec2bin(num))
+    turn_the_on(leds, dec2bin(num))
+    time.sleep(sleep_time)
