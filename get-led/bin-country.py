df35a70f48b728ba589459af1fd689bb4e7dad64
Kirill Ivaniushin <kiril.iva.sml@gmail.com>
2026-09-22 16:08:16 +0300

    Update bin-country.py
    Second task

--- get-led/bin-country.py
+++ get-led/bin-country.py
@@ -30,7 +30,7 @@ num = 0
 sleep_time = 0.2
 
 if GPIO.input(up):
-   if num<256:
+   if num<255:
     num+=1
     print(num, dec2bin(num))
 
@@ -49,7 +49,7 @@ if GPIO.input(down):
           turn_the_on(leds, dec2bin(num))
           time.sleep(sleep_time)
       else:
-          n = 0
+          num = 0
           print(num, dec2bin(num))
           turn_the_on(leds, dec2bin(num))
           time.sleep(sleep_time)
