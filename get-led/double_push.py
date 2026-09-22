f6142350f65a62e88c0ef45d180e42325a1426c2
kirilivasml-glitch <kiril.iva.sml@gmail.com>
2026-09-22 18:34:06 +0300

    65

--- get-led/double_push.py
+++ get-led/double_push.py
@@ -4,9 +4,7 @@ import RPi.GPIO as GPIO
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
-def turn_the_on(leds, num_2):
-        for i in range(8):
-        GPIO.output(leds[i], num_2[i])
+    
 
 
 
@@ -14,46 +12,44 @@ def turn_the_on(leds, num_2):
 GPIO.setmode(GPIO.BCM)
 
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
-up = 10
-down = 9
+up = 9
+down = 10
+num = 0
 
 GPIO.setup(leds, GPIO.OUT)
 
 GPIO.output(leds, 0)
 
-GPIO.setup([up,down] , GPIO.IN)
+GPIO.setup([up,down], GPIO.IN)
 
-num = 0
+
+
 
 sleep_time = 0.2
 
-if GPIO.input(up):
-   if num<255:
-    num+=1
-    print(num, dec2bin(num))
-
-    turn_the_on(leds, dec2bin(num) )
-    time.sleep(sleep_time)
-   else:
-       num = 0
+while True:
+    if GPIO.input(up) and GPIO.input(down):
+       n=255
        print(num, dec2bin(num))
-       turn_the_on(leds, dec2bin(num))
        time.sleep(sleep_time)
 
-if GPIO.input(down):
-      if num>0:
+    elif GPIO.input(up):
+        if num<255:
+          num+=1
+          print(num, dec2bin(num))
+          time.sleep(sleep_time)
+        else:
+          num =0
+          print(num, dec2bin(num))
+          time.sleep(sleep_time)
+    elif GPIO.input(down):
+        if num>0:
           num-=1
           print(num, dec2bin(num))
-          turn_the_on(leds, dec2bin(num))
           time.sleep(sleep_time)
-      else:
+        else:
           num = 0
           print(num, dec2bin(num))
-          turn_the_on(leds, dec2bin(num))
           time.sleep(sleep_time)
-
-if GPIO.input(down) and GPIO.input(up):
-    num = 255
-    print(num, dec2bin(num))
-    turn_the_on(leds, dec2bin(num))
-    time.sleep(sleep_time)
+    GPIO.output(leds, dec2bin(num))
+        
